8114d6ef16eb6f8227aa40815e0e3ac85f9224c9
HendersonTyler <henderson.tyler@outlook.com>
2019-10-23 17:28:14 +1000

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://scontent-syd2-1.cdninstagram.com/vp/4190058411fd538eca487caa98d8a074/5E2ADE5D/t51.2885-19/s150x150/59381178_2348911458724961_5863612957363011584_n.jpg?_nc_ht=scontent-syd2-1.cdninstagram.com"
+                class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>waitgram</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>6,083</strong> posts</div>
+                <div class="pr-5"><strong>314m</strong> followers</div>
+                <div class="pr-5"><strong>221</strong> following</div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
